8a5e7255bdf0b1d9b4627c389122a12fd49dc451
agilanc <chandran.agilan@rabobank.nl>
2019-07-17 23:15:38 +0530

    chore: learnings for autobind, props, states

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import autoBind from 'react-autobind';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//component creation
+
+export default class App extends Component {
+
+  //to creat the state we need the constructor
+
+  constructor() {
+    super(); // react to extend the component and retain this inside constructor
+    this.state = {
+      name: 'virat',
+      hundreds: 25
+    }
+
+    autoBind(this, 'updateName', 'increment', 'decrement'); // automatically bind this for all methods. we mentio the method name to bind 'this' specifically
+  }
+
+  componentDidMount() { // this lifecyle used to embed other framework or to handle ajax request
+    console.log('component mounted')
+  }
+
+  updateName(e) {//default method in react. setState is method and we need to pass the object as argument. It wont mutate and create as new reference.    
+    this.setState({ 
+      name: e.target.value
+    })
+  }
+
+  increment() {
+    this.setState({
+      hundreds: this.state.hundreds + 1
+    })
+  }
+
+  decrement() {
+    this.setState({
+      hundreds: this.state.hundreds - 1
+    })
+  }
+
+  render() {
+
+    const 
+      name = this.props.name,
+      age = this.props.age;
+
+    // to make onChange event work we need to use arrow function or use bind. Other option is auto bind from react
+    return (
+      <div>
+        <h1>My app name is {name}</h1>
+        <h1>My app age is {age}</h1>
+
+        <h3>Player name is: {this.state.name}</h3>
+        <h3>ODI hundreds: {this.state.hundreds}</h3>
+
+        <input onChange={this.updateName}/>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )    
+  }
 }
 
-export default App;
+//check type of props
+
+App.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number
+}
+
+App.defaultProps = {
+  age: 10
+}
+
+//stateless component creation
+
+/*const App = function() {
+  return (
+    <div>
+      <h1>stateless Component</h1>
+    </div>
+  )
+}
+
+export default App;*/
+
